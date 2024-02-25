d357d06ddf4b77d6d85e8d948fdf0acc0409356c
Shubhamashish1310 <98949989+Shubhamashish1310@users.noreply.github.com>
2024-02-25 23:32:50 +0530

    codes

--- ASSIGNMENTS/practice 2.js
+++ ASSIGNMENTS/practice 2.js
@@ -1,19 +1,12 @@
-// let s = "     shubham    is a good b    boy   ";
-// console.log(s.trim());
+let twoSum = function(nums, target) {
+    let n = nums.length;
+    for(let i = 0; i <= n-2; i++) {
+        for(let j = i + 1; j < n; j++) {
+            if(nums[i] + nums[j] == target) {
+                return [i, j];
+            }
+        }
+    }
+};
 
-
-greet ("shubham")
-
-
-
-
-function greet (name) {
-    console.log("hello " + name);
-}
-
-var greet = (name) => {
-    console.log("welcome " + name);
-}
-
-greet("shubham")
-
+console.log(twoSum([3,2,4],6));
