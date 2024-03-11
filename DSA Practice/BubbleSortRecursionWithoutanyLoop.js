729ca38c50c45195bcd8a64566adf3588c8a1a65
Shubhamashish1310 <98949989+Shubhamashish1310@users.noreply.github.com>
2024-03-11 20:20:45 +0530

    dsa

--- DSA Practice/BubbleSortRecursionWithoutanyLoop.js
+++ DSA Practice/BubbleSortRecursionWithoutanyLoop.js
@@ -1,24 +1,41 @@
-function bubbleSortRecursive(arr, index1, index2) {
-   
-if(index1===0){
-    return arr
-}
-if(arr[index1]<arr[index2]){
-    let temp = arr[index1]
-    arr[index1] = arr[index2]
-    arr[index2] = temp
-    bubbleSortRecursive(arr,index1-1,index2-1)
-
-    
-}
-else{
-    bubbleSortRecursive(arr,index1-1,index2)
-}
-return arr
-}
-
-// Example usage
-const arr = [64, 34, 25, 12, 22, 11, 90];
-
-
-console.log(bubbleSortRecursive(arr,arr.length-1,arr.length-2));
+// const bubbleSort = function (arr) {
+//     let n = arr.length;
+//     let isSwapped = false;
+//     for (let i = n - 1; i > 0; i--) {
+//       for (let j = 0; j < i; j++) {
+//         if (arr[j] > arr[j + 1]) {
+//           [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+//           isSwapped = true;
+//         }
+//       }
+//       if (isSwapped == false) return;
+//     }
+//     return arr;
+//   };
+  // console.log(bubbleSort(arr))
+  
+  // using recursion
+  
+  const bubbleSortRecursion = function (arr) {
+    let n = arr.length;
+    solve(arr, n - 1, 0, false);
+    return arr;
+  };
+  
+  const solve = function (arr, i, j, isSwapped) {
+    if (i == 1) return;
+    if (j == i) {
+      if (isSwapped == false) return;
+      i--;
+      j = 0;
+      isSwapped = false;
+    }
+    if (arr[j] > arr[j + 1]) {
+      [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      isSwapped = true;
+    }
+    solve(arr, i, j + 1, isSwapped);
+  };
+  let arr = [2,1,4,3,6,5];
+  
+  console.log(bubbleSortRecursion(arr));
