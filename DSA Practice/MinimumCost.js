503ce73c4555cfa453b7094182a446d2a39c5aa0
Shubhamashish1310 <98949989+Shubhamashish1310@users.noreply.github.com>
2024-03-14 21:53:20 +0530

    partitian algo

--- DSA Practice/MinimumCost.js
+++ DSA Practice/MinimumCost.js
@@ -1,18 +1,18 @@
 function minimumcost(arr){
     
     let totalsum= 0;
-    for(let i in arr){
-        totalsum += arr[i]
+    for(let i in arr){     //n+1
+        totalsum += arr[i]   //n
     }
-    
+    arr = arr.sort((a,b)=>b-a) //n
     let totalcost=0;
-    for(let j in arr){
-        totalcost+=totalsum
-        totalsum-=arr[j]
+    for(let j in arr){   //n+1
+        totalcost+=totalsum //n
+        totalsum-=arr[j]    //n
     }
     
     return totalcost
 }
 
 
-console.log(minimumcost([6,4,1]));
+console.log(minimumcost([6,4,1,8]));
---
+++ DSA Practice/PartitianAlgo.js
@@ -0,0 +1,29 @@
+
+// PARTITIAN ALGORITHM or also known as Dutch National Flag Algorithm
+function partitianalgo(arr,pivot){
+    let n = arr.length
+    let i =0;j=0;k=n-1; // k is high and j is low
+
+    while(i<=k){
+        if(arr[i]>pivot){    //if elemnt is greater than pivot
+            let temp = arr[i]
+            arr[i]=arr[k]
+            arr[k]=temp
+            k--
+        }
+        else if(arr[i]<pivot){   //if element is less than pivot
+            let temp =arr[i]
+            arr[i]=arr[j]
+            arr[j]=temp
+            i++
+            j++
+        }
+        else{          //if element is equal to pivot
+            i++
+        }
+    }
+
+return arr
+}
+
+console.log(partitianalgo([1,2,3,4,5,6,7,8,9,10],5));
