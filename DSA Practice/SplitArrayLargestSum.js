527614a65f59f5971f7aa72ee94c7bac19f08bfd
Shubhamashish1310 <98949989+Shubhamashish1310@users.noreply.github.com>
2024-03-28 23:38:43 +0530

    dsa

---
+++ DSA Practice/SplitArrayLargestSum.js
@@ -0,0 +1,48 @@
+/**
+ * @param {number[]} weights
+ * @param {number} days
+ * @return {number}
+ */
+//its same as capacity to ship
+
+function candone(weights,mid,days){
+    let tw=0;
+    let daysc=0
+    for(let num of weights){
+        if(tw+num>mid){
+            daysc++
+            tw=0
+        }
+        
+            tw+=num
+        
+    }
+    daysc++
+    return daysc<=days
+ }
+var shipWithinDays = function(weights, days) {
+    let n = weights.length
+    let totalweight=0
+    let maxweight = Math.max(...weights)
+
+    for(let s of weights){
+        totalweight+=s
+    }
+
+    let low = maxweight
+    let high = totalweight;
+let ans = high;
+    while(high>=low){
+        let mid = low + Math.floor((high - low) / 2);
+        if(candone(weights,mid,days)){
+            ans =mid
+            high=mid-1
+        }
+        else{
+            low = mid+1
+        }
+    }
+    return ans 
+};
+
+console.log(shipWithinDays([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5));
